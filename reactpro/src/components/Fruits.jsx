a2b1c4fc260d118f936ca5f9cd3335fe9258f855
Kelly <dumpkelly180@gmail.com>
2024-11-05 15:16:28 +0800

    rendering an array of object in a react component to a list item

--- reactpro/src/components/Fruits.jsx
+++ reactpro/src/components/Fruits.jsx
@@ -1,12 +1,13 @@
 export default function Fruits(){
-    const fruits = ["Apple", "Mango", "Banana"];
-    return (
-    <div>
+   // const fruits = ["Apple", "Mango", "Banana"];
+   const fruits = [
+    {name:"Apple", price:10, emoji:"🍎"},
+    {name:"Mango", price:7, emoji:"🥭"},
+    {name:"Orange", price:2, emoji:"🍊"},
+   ];
+    return <div>
         <ul>
-        {fruits.map((fruit) => (
-        <li key={fruit}>{fruit}</li>
-       ))}
-       </ul>
+            {fruits.map(fruit => <li key={fruit.name}> {fruit.emoji}{fruit.name} {"$"} {fruit.price}</li>)}
+        </ul>
     </div>
-    );
 }
