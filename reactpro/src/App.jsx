23d3c8791046c992f6325691280d3e670f468601
Kelly <dumpkelly180@gmail.com>
2024-11-06 14:43:10 +0800

    Using ternary operators in react (using components in ternary operator

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -1,10 +1,10 @@
-import Hello from "./components/Hello"
+import Hello from "./components/Hello";
 import Fruits from "./components/Fruits";
 import ConditionalComponent from "./components/ConditionalCompnent";
 function App() {
   return (
     <div className="App">
-      <ConditionalComponent/>
+      <ConditionalComponent />
     </div>
   );
 }
--- reactpro/src/components/ConditionalCompnent.jsx
+++ reactpro/src/components/ConditionalCompnent.jsx
@@ -2,12 +2,7 @@ import Welcome from "./Welcome";
 import Code from "./Code";
 
 export default function ConditionalComponent() {
-    let message;
-    const display = false;
-    if (display ){
-        message = <h1>Message one</h1>
-    } else {
-    message = <h1>Message Two</h1>
-    }
-    return message;
+  const display = false;
+
+  return display ? <Welcome /> : <Code />;
 }
