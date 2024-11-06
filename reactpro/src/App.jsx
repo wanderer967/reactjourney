03a28eba6a12af76c7317b32e76c669068dbb428
Kelly <dumpkelly180@gmail.com>
2024-11-06 18:04:01 +0800

    using state in react

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -2,10 +2,11 @@ import Hello from "./components/Hello";
 import Fruits from "./components/Fruits";
 import ConditionalComponent from "./components/ConditionalCompnent";
 import Message from "./components/Message";
+import Counter from "./components/Counter";
 function App() {
   return (
     <div className="App">
-      <Message />
+      <Counter />
     </div>
   );
 }
