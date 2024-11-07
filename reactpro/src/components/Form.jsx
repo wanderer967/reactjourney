67efd6bdb302d1c02e3413538e9a9d785942a5ee
Kelly <dumpkelly180@gmail.com>
2024-11-07 14:34:15 +0800

    Handling Form Submission In React

--- reactpro/src/components/Form.jsx
+++ reactpro/src/components/Form.jsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 
 export default function Form() {
   const [name, setName] = useState({ firstName: "", lastName: "" });
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(name);
+  }
 
   return (
     <div>
@@ -17,6 +21,7 @@ export default function Form() {
           type="text"
           value={name.lastName}
         />
+        <button onClick={(e) => handleSubmit(e)}>Add</button>
       </form>
     </div>
   );
