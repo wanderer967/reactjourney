3e7ed6769773c6b6a4e2560a2a118ede5db17a76
Kelly <dumpkelly180@gmail.com>
2024-11-07 16:11:33 +0800

    styling a component using stylesheet

--- todoappreactpro/todoapp/src/components/InLineComponent.jsx
+++ todoappreactpro/todoapp/src/components/InLineComponent.jsx
@@ -1,8 +1,8 @@
+import "../css/style.css";
 export default function InLineComponent() {
-  const header = { color: "blue", fontSize: "30px" };
   return (
     <div>
-      <h1 style={header}>Initial Component</h1>
+      <h1 className="header">Initial Component</h1>
     </div>
   );
 }
