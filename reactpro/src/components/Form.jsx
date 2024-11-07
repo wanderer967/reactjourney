4fe9d328610a331950f0a913e2b7006d0ce95438
Kelly <dumpkelly180@gmail.com>
2024-11-07 14:27:51 +0800

    Handling Multiple Inputs in React

--- reactpro/src/components/Form.jsx
+++ reactpro/src/components/Form.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 
 export default function Form() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState({ firstName: "", lastName: "" });
 
   return (
     <div>
+      {name.firstName} - {name.lastName}
       <form>
         <input
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => setName({ ...name, firstName: e.target.value })}
           type="text"
-          value={name}
+          value={name.firstName}
+        />
+        <input
+          onChange={(e) => setName({ ...name, lastName: e.target.value })}
+          type="text"
+          value={name.lastName}
         />
       </form>
     </div>
