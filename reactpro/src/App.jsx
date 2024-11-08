b091e35f5234a941960ef931c49e8a276fe71638
wanderer967 <dumpkelly180@gmail.com>
2024-11-08 23:57:36 +0800

    todo

--- reactpro/src/App.jsx
+++ reactpro/src/App.jsx
@@ -1,8 +1,3 @@
-import Hello from "./components/Hello";
-import Fruits from "./components/Fruits";
-import ConditionalComponent from "./components/ConditionalCompnent";
-import Message from "./components/Message";
-import Counter from "./components/Counter";
 import Form from "./components/Form";
 function App() {
   return (
--- todoappreactpro/todoapp/src/components/OutLineComponent.jsx
+++ todoappreactpro/todoapp/src/components/OutLineComponent.jsx
@@ -2,7 +2,7 @@ import styles from "./outlinecomponent.module.css";
 export default function OutLineComponent() {
   return (
     <div>
-      <h1 className={styles.header}>This is outline component test commit</h1>
+      <h1 className={styles.header}>This is outline component test </h1>
     </div>
   );
 }
