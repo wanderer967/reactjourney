d5f7186ef71e1634059aa27f0779ccfe5b4708e1
wanderer967 <dumpkelly180@gmail.com>
2024-11-08 23:58:11 +0800

    todo

--- todoappreactpro/todoapp/src/components/OutLineComponent.jsx
+++ todoappreactpro/todoapp/src/components/OutLineComponent.jsx
@@ -2,7 +2,7 @@ import styles from "./outlinecomponent.module.css";
 export default function OutLineComponent() {
   return (
     <div>
-      <h1 className={styles.header}>This is outline component test </h1>
+      <h1 className={styles.header}>This is outline component </h1>
     </div>
   );
 }
