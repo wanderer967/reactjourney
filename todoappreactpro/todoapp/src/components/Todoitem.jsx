00ef7a4ae09e406e0311e05b09a4b1047da4efda
Kelly <dumpkelly180@gmail.com>
2024-11-12 17:48:48 +0800

    adding a delete button

--- todoappreactpro/todoapp/src/components/Todoitem.jsx
+++ todoappreactpro/todoapp/src/components/Todoitem.jsx
@@ -2,7 +2,13 @@ import styles from "./todoitem.module.css";
 export default function Todoitem({ item }) {
   return (
     <div className={styles.item}>
-      <div className={styles.itemName}>{item}</div>
+      <div className={styles.itemName}>
+        {item}
+        <span className={styles.delete}>
+          <button>x</button>
+        </span>
+      </div>
+
       <hr className={styles.line} />
     </div>
   );
